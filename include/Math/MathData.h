a22fa380764b8d8fb7c70643428b4eafff2053c9
Christian <ckorreng@yahoo.com>
2025-08-26 07:53:42 +0200

    minor math correction

--- include/Math/MathData.h
+++ include/Math/MathData.h
@@ -619,14 +619,6 @@ inline float Dot3D(const Vec4& v1, const Vec4& v2)
 	return(v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
 }
 
-inline Vec4 Cross(const Vec4& v1, const Vec4& v2)
-{
-	return Vec4((v1.y * v2.z - v1.z * v2.y),
-		(v1.z * v2.x - v1.x * v2.z),
-		(v1.x * v2.y - v1.y * v2.x),
-		0.0f);
-}
-
 inline float Dist(const Vec4& v1, const Vec4& v2)
 {
 	return (sqrt((v1.x - v2.x) * (v1.x - v2.x)
